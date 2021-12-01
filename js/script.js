a550cd688d2f4418a62b7befe61af8ff295cbb34
Glitch (lean-magenta-weather) <none>
2021-12-01 23:22:15 +0000

    🍵📊 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-
+$("play").click(function() {
+  let choice = $(".input").val();
+  $(".userChoice").text(choice);
+  let playerChoice;
+  
+  if(playerChoice === 'rock'){
+    playerChoice
+  }
+  
+let rock =1;
+let paper =2;
+let scisors =3;
+  
+});
